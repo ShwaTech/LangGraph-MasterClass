1c6ae02f0f7557c8a549a6bd531108e9e34cac39
ShwaTech <shwakesh13579@gmail.com>
2025-09-14 23:41:21 +0300

    StateGraph - Slightly Complex State

---
+++ 05-State-DeepDive/complex_state.py
@@ -0,0 +1,56 @@
+from typing import TypedDict, List
+from langgraph.graph import END, StateGraph
+
+
+class BasicState(TypedDict):
+    count: int
+    sum: int
+    history: List[int]
+
+
+def increament(state: BasicState) -> BasicState:
+    new_count = state["count"] + 1
+    new_sum = state["sum"] + new_count
+    return {
+        "count": new_count,
+        "sum": new_sum,
+        "history": state["history"] + [new_count]
+    }
+
+
+def should_continue(state: BasicState) -> str:
+    if state["count"] < 5:
+        return "Continue"
+    return "Exit"
+
+
+graph_builder = StateGraph(BasicState)
+
+graph_builder.add_node("increment", increament)
+
+graph_builder.set_entry_point("increment")
+
+graph_builder.add_conditional_edges(
+    "increment",
+    should_continue,
+    {
+        "Continue": "increment",
+        "Exit": END
+    }
+)
+
+graph = graph_builder.compile()
+
+print(graph.get_graph().draw_mermaid())
+
+
+state = {
+    "count": 0,
+    "sum": 0,
+    "history": []
+}
+
+response = graph.invoke(state)
+
+print(response)
+
