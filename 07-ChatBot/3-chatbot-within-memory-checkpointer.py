b9fe0805b098ec2d00193dd730b03a2a69b13bb4
ShwaTech <shwakesh13579@gmail.com>
2025-09-15 02:26:15 +0300

    ChatBot - Basic ChatBot With Tools and Memory Added

---
+++ 07-ChatBot/3-chatbot-within-memory-checkpointer.py
@@ -0,0 +1,54 @@
+from typing import TypedDict, Annotated
+from langgraph.graph import StateGraph, add_messages, END
+from langchain_groq import ChatGroq
+from langchain_core.messages import AIMessage, HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+memory = MemorySaver()
+
+class BasicChatState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+def chatbot(state: BasicChatState):
+    return {
+        "messages": [llm.invoke(state["messages"])]
+    }
+
+
+graph_builder = StateGraph(BasicChatState)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile(checkpointer=memory)
+
+config = {
+    "configurable": {
+        "thread_id": 1
+    }
+}
+
+
+while True:
+    user_input = input("👨 User: ")
+    
+    if user_input in ["exit", "quit", "bye", "goodbye", "q", "end"]:
+        break
+    
+    response = graph.invoke({
+        "messages": [HumanMessage(content=user_input)]
+    }, config=config)
+    
+    print("🤖 Assistant: ", response["messages"][-1].content)
+
